92b2477d81bab92bc9f2876ade18f1c0e18266d5
fenglvv <1260386538@qq.com>
2024-02-01 10:20:31 +0100

    Update GameSetting.cs

---
+++ GameSetting.cs
@@ -0,0 +1,18 @@
+﻿namespace PixelForge; 
+
+public static class GameSetting {
+	public const int WindowWidth = 800;
+	public const int WindowHeight = 700;
+	public const string Name = "PixelForge";
+	public const string ProjectPath = @"../../../";
+	
+	public const int ChunkSize = 16;
+	
+	public static DateTime StartTime;
+
+	
+	public static void OnLoad() {
+		StartTime = DateTime.UtcNow;
+	}
+	
+}
