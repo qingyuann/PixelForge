91ec18c3ea4b08dc2cfcd0121cc93247486fb0de
fenglvv <1260386538@qq.com>
2024-02-21 20:21:43 +0100

    Update Transform.cs

--- Tools/Transform.cs
+++ Tools/Transform.cs
@@ -18,4 +18,20 @@ public static class Transform {
 		screenPos.Y = 1 - screenPos.Y;
 		return screenPos;
 	}
+	
+	public static Vector2 ScreenToWorld( Vector2 position ) {
+		CameraSystem.GetMainCamPara( out var camPos, out var camScale );
+		position.Y = 1 - position.Y;
+		position = position * new Vector2( 2, 2 ) - new Vector2( 1, 1 );
+		position *= camScale;
+		position = new Vector2( position.X / GlobalVariable.XUnit, position.Y / GlobalVariable.YUnit );
+		position += new Vector2( camPos.X, camPos.Y );
+		return position;
+	}
+	
+	public static Vector2 PixelToWorld( Vector2 position ) {
+		var ratio = new Vector2( position.X / GameSetting.WindowWidth, position.Y / GameSetting.WindowHeight );
+		var pos = ScreenToWorld( ratio );
+		return pos;
+	}
 }
