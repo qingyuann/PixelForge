2ef042c774f97116aa9c1848fd482c03abc8a331
fenglvv <1260386538@qq.com>
2024-02-25 18:12:15 +0100

    Revert "Revert "Accelerate image loading process""
    This reverts commit d36530a24dc10179a59ef1fa10423ba3b01d44a5.

--- Global/GlobalVariable.cs
+++ Global/GlobalVariable.cs
@@ -1,12 +1,14 @@
 ﻿using Silk.NET.GLFW;
 using Silk.NET.OpenGL;
+using Silk.NET.SDL;
+using Silk.NET.Windowing;
 
 namespace PixelForge;
 
 public static class GlobalVariable {
 	public static GL GL;
 	public static Contexts Contexts;
-	
+	public static IWindow Window;
 	// 1 unit = 1000 pixels
 	// world_size_x * XUnit = screen_ratio_x
 	public static float UnitPixel = 1000;
